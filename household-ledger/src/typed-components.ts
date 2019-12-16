6d92a614204ebb52681ba1b37174f97e40c6b705
doohyun <ewb.doohyun@gmail.com>
2019-12-16 21:43:01 +0900

    initialize project

---
+++ household-ledger/src/typed-components.ts
@@ -0,0 +1,19 @@
+import * as styledComponents from "styled-components";
+
+interface IThemeInterface {
+    blurColor: string;
+    greyColor: string;
+    yellowColor: string;
+    greenColor: string;
+  }
+  
+  const {
+    default: styled,
+    css,
+    createGlobalStyle,
+    keyframes,
+    ThemeProvider
+  } = styledComponents as styledComponents.ThemedStyledComponentsModule<IThemeInterface>;
+  
+  export { css, createGlobalStyle, keyframes, ThemeProvider };
+  export default styled;
